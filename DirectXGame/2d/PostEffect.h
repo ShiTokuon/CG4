d712c48874c50803f6f1a2e6a5f316ea8cc0570c
ShiTokuon <k021g1160@g.neec.ac.jp>
2023-07-06 16:31:31 +0900

    03_04マルチパスレンダリング

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -1,29 +1,49 @@
 #pragma once
 #include "Sprite.h"
 class PostEffect :
-    public Sprite
+	public Sprite
 {
+private: // 静的メンバ変数
+	static const float clearColor[4];
 public:
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    PostEffect();
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	PostEffect();
 
-    /// <summary>
-    /// 初期化
-    /// </summary>
-    void Initialize();
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
 
-    /// <summary>
-    /// 描画コマンドの発行
+	/// <summary>
+	/// 描画コマンドの発行
+	/// </summary>
+	/// <param name="cmdList">コマンドリスト</param>
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+	/// <summary>
+	/// シーン描画前処理
+	/// </summary>
+	/// <param name="cmdList">コマンドリスト</param>
+	void PreDrawScene(ID3D12GraphicsCommandList* cmdList);
+
+	/// <summary>
+    /// シーン描画前処理
     /// </summary>
     /// <param name="cmdList">コマンドリスト</param>
-    void Draw(ID3D12GraphicsCommandList* cmdList);
+	void PostDrawScene(ID3D12GraphicsCommandList* cmdList);
 
 private: // メンバ変数
-    // テクスチャバッファ
-    ComPtr<ID3D12Resource> texBuff;
-    // SRV用デスクリプタヒープ
-    ComPtr<ID3D12DescriptorHeap>descHeapSRV;
+	// テクスチャバッファ
+	ComPtr<ID3D12Resource> texBuff;
+	// SRV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap>descHeapSRV;
+	// 深度バッファ
+	ComPtr<ID3D12Resource> depthBuff;
+	// RTV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> descHeapRTV;
+	// DSV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> descHeapDSV;
 };
 
