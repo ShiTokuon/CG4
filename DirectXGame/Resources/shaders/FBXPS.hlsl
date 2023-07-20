5b9b2b8c1830438f75df75743fb6b7dac0473d1d
ShiTokuon <k021g1160@g.neec.ac.jp>
2023-07-20 12:17:04 +0900

    02評価課題2

--- DirectXGame/Resources/shaders/FBXPS.hlsl
+++ DirectXGame/Resources/shaders/FBXPS.hlsl
@@ -14,15 +14,15 @@ SamplerState smp:register(s0);
 PSOutput main(VSOutput input) : SV_TARGET
 {
 	PSOutput output;
-	// テクスチャマッピング
-	float4 texcolor = tex.Sample(smp,input.uv);
-	// Lamgert反射
-	float3 light = normalize(float3(1, -1, 1)); // 右下奥　向きのライト
-	float diffuse = saturate(dot(-light, input.normal));
-	float brightness = diffuse + 0.3f;
-	float4 shadecolor = float4(brightness, brightness, brightness, 1.0f);
-	// 陰影とテクスチャの色を合成
-	output.target0 = shadecolor * texcolor;
-	output.target1 = float4(1 - (shadecolor * texcolor).rgb, 1);
-	return output;
+    // テクスチャマッピング
+    float4 texcolor = tex.Sample(smp,input.uv);
+    // Lamgert反射
+    float3 light = normalize(float3(1, -1, 1)); // 右下奥　向きのライト
+    float diffuse = saturate(dot(-light, input.normal));
+    float brightness = diffuse + 0.3f;
+    float4 shadecolor = float4(brightness, brightness, brightness, 1.0f);
+    // 陰影とテクスチャの色を合成
+    output.target0 = shadecolor * texcolor;
+    output.target1 = float4((shadecolor * texcolor).rgb, 1);
+    return output;
 }
--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -7,24 +7,26 @@ SamplerState smp : register(s0);      // 0番スロットに設定されたサ�
 float4 main(VSOutput input) : SV_TARGET
 {
 	float4 colortex0 = tex0.Sample(smp,input.uv);
-	float4 colortex1 = tex1.Sample(smp, input.uv);
+	float4 colortex1 = tex1.Sample(smp,input.uv);
 
-	float4 color = colortex0;
+	float4 total = 0;
+	for (float x = -1; x < 2; x++)
+	{
+		for (float y = -1; y < 2; y++)
+		{
+			total += tex1.Sample(smp, input.uv + float2(x / 640, y / 360));
+		}
+	}	
+	float4 avg = total / 9;
+
+	float4 color = avg;
 	if (fmod(input.uv.y, 0.1f) < 0.05f) {
-		color = colortex1;
+		color = 1 - colortex0;
 	}
-	//float4 total = 0;
-	//for (float x = -1; x < 2; x++)
-	//{
-	//	for (float y = -1; y < 2; y++)
-	//	{
-	//		total += tex.Sample(smp, input.uv + float2(x / 640, y / 360));
-	//	}
-	//}
+
 	//float4 texcolor = tex.Sample(smp,input.uv);
 	//float4 texcolor2 = tex.Sample(smp, input.uv + float2(1 / 640,0));
 	//float4 total = texcolor + texcolor2;
-	//float4 avg = total / 9;
 
 	//return float4(avg.rgb, 1);
 	//return float4(texcolor.rgb, 1);
